4e5e9cae2c36b0ee0b7deeac7d3d8fb284af63cb
Fernando Briano <fernando@picandocodigo.net>
2014-04-09 03:51:37 -0300

    Checks if file exists before downloading it Adds README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Image grabber
+
+For downloading images from htlm indexes such as
+http://isis.bibna.gub.uy/imagenes/
+
+Prototype project, **Work in progress**
+
+To use it, clone the repo and run:
+```bash
+ruby app.rb [url]
+```
--- app.rb
+++ app.rb
@@ -10,15 +10,17 @@ class GrabImages
   end
 
   def folder_images(folder = nil)
-    uri = (folder ? @uri + folder : @uri)
+    uri = (folder ? "#{@uri}#{folder}/" : @uri)
     begin
       open(uri) do |f|
         f.each_line do |t|
+          # Check for image
           if match = t.match(/href=\"([\w\d_]*\.(jpg|jpeg|png|gif|bmp))\"/)
             save_image(match[1], folder)
           end
 
-          if match = ( t.match(/href=\"([a-z^\._\-0-9]+\/)/))
+          # Probably have to change folder:
+          if match = t.match(/href=\"([a-z^\._\-0-9]+)\/\">\ *[^(Parent)]/)
             change_folder(match[1].gsub('/', ''))
           end
         end
@@ -27,23 +29,23 @@ class GrabImages
       error_msg(e.to_s)
       puts 'Waiting to retry in 10 seconds...'
       sleep(10)
+      folder_images
     rescue => e
       error_msg(e.to_s)
-      puts e.backtrace
     end
   end
 
   def save_image(img, folder = nil)
     folder = (folder ? "#{@folder}/#{folder}" : @folder)
     open(folder ? "#{folder}/#{img}" : img, 'wb') do |file|
-      file << open("#{@uri}/#{img}").read
+      file << open("#{@uri}/#{img}").read unless File.exists?("#{folder}/#{img}")
     end
     puts('File saved: '.fg('#00ff00') + "#{img} in /#{folder.fg('#71b9f8')}")
   end
 
   def change_folder(folder)
     puts 'Changing folders to ' + folder.fg('#71b9f8')
-    Dir.mkdir("#{@folder}/#{folder}") unless Dir.exist?(folder)
+    Dir.mkdir("#{@folder}/#{folder}") unless Dir.exist?("#{@folder}/#{folder}")
     folder_images(folder) unless folder == ''
   end
 
